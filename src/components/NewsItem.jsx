9456643ce51aef51a5d3755557cd23f2cb3491ef
Apoorva Kesarwani <apoorvakesarwani001@gmail.com>
2023-08-27 01:53:05 +0530

    first commit

---
+++ src/components/NewsItem.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const NewsItem = (props)=> {
+        let { title, description, imageUrl, newsUrl, author, date, source } = props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }> 
+                        <span className="badge rounded-pill bg-danger"> {source} </span>
+                    </div>
+                    <img src={!imageUrl ? "https://t4.ftcdn.net/jpg/04/73/25/49/360_F_473254957_bxG9yf4ly7OBO5I0O5KABlN930GwaMQz.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}  </h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-danger" style={{color:"black"}}>Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+     
+}
+
+export default NewsItem
---
+++ src/components/navbar.jsx
@@ -0,0 +1,47 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+
+  return (
+    <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark" aria-label="Fifth navbar example">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Newsance</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarsExample05">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/"style={{color:"red"}}  >Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/business" style={{color:"red"}}>Business</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/entertainment" style={{color:"red"}}>Entertainment</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/general" style={{color:"red"}}>General</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/health" style={{color:"red"}}>Health</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/science" style={{color:"red"}}>Science</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/technology" style={{color:"red"}}>Technology</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/sports" style={{color:"red"}}>Sports</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+
+export default Navbar
