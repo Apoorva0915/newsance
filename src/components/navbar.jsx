1641b44be757abc9eeea6dfccead2aadab1d7ea0
Apoorva Kesarwani <apoorvakesarwani001@gmail.com>
2023-08-27 02:03:31 +0530

    first commit

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark" aria-label="Fifth navbar example">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Newsance</Link>
+        <Link className="navbar-brand" to="/newsance">Newsance</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -13,7 +13,7 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarsExample05">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0" >
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/"style={{color:"red"}}  >Home</Link>
+              <Link className="nav-link active" aria-current="page" to="/newsance"style={{color:"red"}}  >Home</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/business" style={{color:"red"}}>Business</Link>
